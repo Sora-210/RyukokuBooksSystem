23cfeff06223409c6949dc6168ce87e7bb22e099
Sora210 <sokominto1231@gmail.com>
2020-06-19 23:28:22 +0900

    comopnents/BooksCard 取得周りの変更

--- FrontEnd/src/components/BooksCard.vue
+++ FrontEnd/src/components/BooksCard.vue
@@ -5,15 +5,13 @@
                 :src="bookImgUrl"
             ></v-img>
             <v-card-title>
-                {{ id }}
+                {{ title }}
             </v-card-title>
             <v-card-text>
-                {{ Title }}
+                {{ author }}
             </v-card-text>
             <v-card-actions>
-                <v-btn
-                    @click="getApi"
-                >
+                <v-btn>
                     借りる
                 </v-btn>
             </v-card-actions>
@@ -26,7 +24,8 @@ export default {
     name:'BooksCard',
     data: function() {
         return {
-            "Title":""
+            "title":"",
+            "author":""
         }
     },
     props:[
@@ -35,20 +34,21 @@ export default {
     computed: {
         bookImgUrl: function() {
             return 'https://cover.openbd.jp/' + this.id + '.jpg'
+        },
+        bookUrl: function() {
+            return '/book/' + this.id
         }
     },
-    methods: {
-        getApi() {
-            console.log('access:' + 'https://api.openbd.jp/v1/get?isbn=' + this.id)
-            this.axios.get('https://api.openbd.jp/v1/get?isbn=' + this.id)
-                .then((res) => {
-                    console.log(res.data)
-                    this.Title = res.data[0].onix.DescriptiveDetail.TitleDetail.TitleElement.TitleText.content
-                })
-                .catch((err) => {
-                    alert(err)
-                })
-        }
+    mounted: function() {
+        this.axios('https://api.openbd.jp/v1/get?isbn=' + this.id)
+            .then((res) => {
+                this.title = res.data[0].summary.title
+                this.author = res.data[0].summary.author
+            })
+            .catch((err) => {
+                alert('Bookデータの取得中にエラーが発生しました')
+                console.log(err)
+            })
     }
 }
 </script>
