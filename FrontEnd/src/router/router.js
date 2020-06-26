5a0f3abc2bb9a696a8604c763beba0db7055d7ad
Sora210 <sokominto1231@gmail.com>
2020-06-26 23:24:51 +0900

    QRCodeリーダーを仮作成

--- FrontEnd/src/router/router.js
+++ FrontEnd/src/router/router.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // view import
 import BookPage from '../view/BookPage.vue'
+import Collection from '../view/Collection.vue'
 import Home from '../view/Home.vue'
 import ErrorNotFound from '../view/ErrorNotFound.vue'
 import Search from '../view/Search.vue'
 import Request from '../view/Request.vue'
+import CodeReader from '../view/CodeReader.vue'
 
 
 Vue.use(Router)
@@ -22,6 +24,14 @@ const routes = [
     {
         path: '/book/:isbn',
         component: BookPage
+    },
+    {
+        path: '/collection/:uuid',
+        component: Collection
+    },
+    {
+        path: '/qrcodereader',
+        component: CodeReader
     },
     {
         path: '/request',
---
+++ FrontEnd/src/view/CodeReader.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <qrcode-stream @decode="onDecode" @init="onInit"></qrcode-stream>
+        <p>{{this.result}}</p>
+        <p>{{this.error}}</p>
+    </div>
+</template>
+<script>
+import { QrcodeStream } from 'vue-qrcode-reader'
+
+export default {
+    name:"CodeReader",
+    data: function() {
+        return {
+            result:"",
+            error:""
+        }
+    },
+    components: {
+        QrcodeStream
+    },
+    methods: {
+        onDecode (result) {
+            this.result = result
+        },
+        async onInit (promise) {
+            try {
+                await promise
+            } catch (error) {
+                if (error.name === 'NotAllowedError') {
+                    this.error = "ERROR: you need to grant camera access permisson"
+                } else if (error.name === 'NotFoundError') {
+                    this.error = "ERROR: no camera on this device"
+                } else if (error.name === 'NotSupportedError') {
+                    this.error = "ERROR: secure context required (HTTPS, localhost)"
+                } else if (error.name === 'NotReadableError') {
+                    this.error = "ERROR: is the camera already in use?"
+                } else if (error.name === 'OverconstrainedError') {
+                    this.error = "ERROR: installed cameras are not suitable"
+                } else if (error.name === 'StreamApiNotSupportedError') {
+                    this.error = "ERROR: Stream API is not supported in this browser"
+                }
+            }
+        }
+    }
+}
+</script>
