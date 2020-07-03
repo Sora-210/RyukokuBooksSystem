a3ed35bf300550576aa7d2c0e670f679b1da118b
sora210 <sokominto1231@gmail.com>
2020-07-04 02:41:24 +0900

    アクセスログの収集

--- ManagementApiServer/app.js
+++ ManagementApiServer/app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const BodyParser = require('body-parser')
 
+const AccessLog = require('./src/Log.js').getLogger('access')
+AccessLog.level = "info"
+
 const app = express()
 app.use(BodyParser())
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    AccessLog.info(req.ip + " - [" + req.method + "] " + req.originalUrl)
     next();
 });
 
