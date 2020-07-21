2458c802df02503f4bc7cd6856f71305236525ac
sora210 <sokominto1231@gmail.com>
2020-07-22 00:36:17 +0900

    エラー系ページの製作

---
+++ FrontEnd/src/view/error/404.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="mt-10">
+        <v-icon size="100">
+            fas fa-exclamation-triangle
+        </v-icon>
+        <h3 class="topText">
+            404 NotFound
+        </h3>
+        <br>
+        <p class="subText">
+            ページが見つかりませんでした
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Error404",
+}
+</script>
+<style scoped>
+div {
+    text-align:center;
+    color:#757575;
+}
+.topText {
+    font-size: 63px;
+}
+.subText {
+    font-size: 23px;
+}
+@media screen and (max-width:425px) {
+    .topText {
+        font-size: 40px;
+    }
+    .subText {
+        font-size: 15px;
+    }
+}
+</style>
---
+++ FrontEnd/src/view/error/500.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="mt-10">
+        <v-icon size="100">
+            fas fa-exclamation-triangle
+        </v-icon>
+        <h3 class="topText">
+            500 Internal Server Error
+        </h3>
+        <br>
+        <p class="subText">
+            管理サーバーで問題が発生しました
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Error500",
+}
+</script>
+<style scoped>
+div {
+    text-align:center;
+    color:#757575;
+}
+.topText {
+    font-size: 63px;
+}
+.subText {
+    font-size: 23px;
+}
+@media screen and (max-width:425px) {
+    .topText {
+        font-size: 40px;
+    }
+    .subText {
+        font-size: 15px;
+    }
+}
+</style>
