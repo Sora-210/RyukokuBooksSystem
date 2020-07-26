bb05906b9815c675ece57ac85bf4e14f68e2d0d1
sora210 <sokominto1231@gmail.com>
2020-07-27 00:16:14 +0900

    queryオプションの作成

--- ManagementApiServer/src/Rental.js
+++ ManagementApiServer/src/Rental.js
@@ -4,6 +4,8 @@ require('date-utils')
 const rentalRouter = express.Router()
 const { CheckAuth } = require('./func/Auth.js')
 const DB = require('../models/index')
+const sequelize = require('sequelize')
+const  DBOP = sequelize.Op
 /*
 ################################
 Rental処理
@@ -23,11 +25,41 @@ rentalRouter.use(CheckAuth)
 // (AUTH)[GET] /
 rentalRouter.get('/', async(req, res) => {
     try {
-        const DBres = await DB.Rental.findAll()
+        const options = {where:{}}
+        
+        // uuid関連処理
+        if (req.query.uuid !== undefined && req.query.uuid !== "") {
+            options.where.uuid = req.query.uuid
+        }
+        // ステータス関連処理
+        if (req.query.status !== undefined && req.query.status !== "") {
+            if (req.query.status === "true") {
+                options.where.return_day = null
+            } else {
+                options.where.return_day = { [DBOP.ne]: null}
+            }
+            
+        }
+        const Count = await DB.Rental.count(options)
+
+        // ソート関連処理
+        if (req.query.sortRow !== undefined && req.query.sortRow !== "") {
+            if (req.query.sortDirection !== undefined && req.query.sortDirection != 1) {
+                options.order = [[req.query.sortRow, "DESC"]]
+            } else {
+                options.order = [[req.query.sortRow, "ASC"]]
+            }
+        }
+        // ページ関連処理
+        if (req.query.page !== undefined && req.query.page !== "") {
+            options.limit = 20
+            options.offset = (req.query.page - 1) * 20
+        }
+        const DBres = await DB.Rental.findAll(options)
         if (DBres.length === 0) {
             throw new CustomError('Nothing')
         }
-        return res.status(200).json(DBres)
+        return res.status(200).json({count:Count,Rentals:DBres})
     } catch(e) {
         switch(e.name) {
             case 'Nothing':
