cd682854f864ab0767f7fa04fe716e573ba77022
Sora210 <sokominto1231@gmail.com>
2020-07-28 00:36:17 +0900

    検索ページ制作

---
+++ FrontEnd/src/components/BooksCardRow.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <v-card @click="Link" class="d-flex .justify-start">
+            <v-row>
+                <v-col cols=5 sm=4 md=2>
+                    <v-img
+                        :src="bookImgUrl"
+                        class="ma-2"
+                    ></v-img>
+                </v-col>
+                <v-col cols=7 sm=8 md=10>
+                    <v-card-title class="title">
+                        {{ title }}
+                    </v-card-title>
+                    <v-card-text class="author">
+                        <p v-for="author in this.authors" :key="author.index">
+                            {{ author }} / 著
+                        </p>
+                    </v-card-text>
+                </v-col>
+            </v-row>
+        </v-card>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'BooksCardRow',
+    data: function() {
+        return {
+            "title":"",
+            "authors":"",
+            "imgId":""
+        }
+    },
+    props:[
+        'id',
+        'uuid'
+    ],
+    computed: {
+        bookImgUrl: function() {
+            return "https://books.google.com/books/content/images/frontcover/" + this.imgId + "?fife=w800-h1200"
+        },
+        bookUrl: function() {
+            return '/collection/' + "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d"
+        }
+    },
+    mounted: function() {
+        this.axios('https://www.googleapis.com/books/v1/volumes?q=isbn:' + this.id)
+            .then((res) => {
+                this.title = res.data.items[0].volumeInfo.title
+                this.authors = res.data.items[0].volumeInfo.authors
+                this.imgId = res.data.items[0].id
+            })
+            .catch((err) => {
+                this.$emit('Error','書籍データ取得に失敗しました')
+                console.log(err)
+            })
+    },
+    methods: {
+        Link() {
+            this.$router.push('/collection/' + this.uuid)
+        }
+    }
+}
+</script>
--- FrontEnd/src/view/Search.vue
+++ FrontEnd/src/view/Search.vue
@@ -6,15 +6,175 @@
             color="deep-purple accent-4"
             elevation="2"
         >
-            動作しません
+            検索できません
         </v-alert>
-        <v-text-field>
-            label="ISBN...."
-        </v-text-field>
+        <v-card>
+            <v-card-title>
+                蔵書一覧 | 検索
+            </v-card-title>
+            <v-divider></v-divider>
+            <v-card-actions>
+                <v-container>
+                    <v-row>
+                        <v-col cols="12" sm="6">
+                            <v-text-field
+                                label="UUID"
+                                append-icon="fas fa-qrcode"
+                                @click:append="isQrDialog = true"
+                                v-model="searchConditions.uuid"
+                            ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6">
+                            <v-text-field
+                                label="NCD"
+                                v-model="searchConditions.ncd"
+                            ></v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col class="d-flex" cols="12" sm="6">
+                            <v-select
+                                append-icon="fas fa-caret-down"
+                                :items="[{text:'UUID',value:'uuid'},{text:'NCD',value:'ncd'},{text:'登録日',value:'registrationData'}]"
+                                v-model="searchConditions.sortRow"
+                                label="並び替え"
+                            ></v-select>
+                        </v-col>
+                        <v-col class="d-flex" cols="12" sm="6">
+                            <v-select
+                                append-icon="fas fa-caret-down"
+                                :items="[{text:'降順',value:0},{text:'昇順',value:1}]"
+                                v-model="searchConditions.sortDirection"
+                                label="順番"
+                            ></v-select>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-card-actions>
+            <v-card-actions>
+                <v-btn color="warning" @click="searchReset">リセット</v-btn>
+                <v-btn color="success" @click="search">検索</v-btn>
+            </v-card-actions>
+            <v-divider></v-divider>
+            <v-card-text>
+                検索結果: {{ targetCollectionCount }} 件
+            </v-card-text>
+            <v-container>
+                <!-- <v-row>
+                    <v-col
+                        cols="6"
+                        sm="3"
+                        v-for="Collection in Collections" :key="Collection.uuid"
+                    >
+                        <BookCard
+                            :id="Collection.isbn"
+                            :uuid="Collection.uuid"
+                            style="margin:5px;"
+                            @Error="Error"
+                        >
+                        </BookCard>
+                    </v-col>
+                </v-row> -->
+                <v-row>
+                    <v-col
+                        cols="12"
+                        v-for="Collection in Collections" :key="Collection.uuid"
+                    >
+                        <BooksCardRow
+                            :id="Collection.isbn"
+                            :uuid="Collection.uuid"
+                            style="margin:5px;"
+                            @Error="Error"
+                        >
+                        </BooksCardRow>
+                    </v-col>
+                </v-row>
+            </v-container>
+            <v-divider></v-divider>
+            <v-card-actions>
+                <div class="text-right">
+                    <v-pagination
+                    v-model="page"
+                    next-icon="fas fa-caret-right"
+                    prev-icon="fas fa-caret-left"
+                    :length="CollectionPage"
+                    ></v-pagination>
+                </div>
+            </v-card-actions>
+        </v-card>
+        <QRreader :isQrDialog="isQrDialog" @dataUp="QrResult" @close="isQrDialog = false" @Error="Error">
+        </QRreader>
     </div>
 </template>
 <script>
+// import BookCard from '../components/BooksCard.vue'
+import BooksCardRow from '../components/BooksCardRow.vue'
+import QRreader from '../components/QRreader.vue'
+
 export default {
-    
+    components: {
+        // BookCard,
+        QRreader,
+        BooksCardRow
+    },
+    data: function() {
+        return {
+            Collections:[],
+            page:1,
+            CollectionPage:0,
+            targetCollectionCount:0,
+            searchConditions: {
+                sortRow:"registrationData",
+                sortDirection:0,
+                uuid:"",
+                ncd:""
+            },
+            isQrDialog:false
+        }
+    },
+    mounted: function() {
+        this.getCollections()
+    },
+    methods: {
+        getCollections() {
+            let query = `?sortRow=${this.searchConditions.sortRow}&sortDirection=${this.searchConditions.sortDirection}`
+            query += `&ncd=${this.searchConditions.ncd}&uuid=${this.searchConditions.uuid}`
+            query += `&page=${this.page}`
+            this.axios.get(this.$store.getters.apiEndpoint + '/collections' + query)
+                .then((res) => {
+                    this.targetCollectionCount = res.data.count
+                    this.CollectionPage = Math.ceil(res.data.count / 20)
+                    this.Collections = res.data.Collections
+                    console.log(this.Collections)
+                })
+                .catch((e) => {
+                    this.$emit('Error',e)
+                })
+        },
+        search: function() {
+            this.page = 1
+            this.getCollections()
+        },
+        searchReset: function() {
+            this.searchConditions = {
+                sortRow:"registrationData",
+                sortDirection:0,
+                uuid:"",
+                ncd:""
+            }
+        },
+        QrResult: function(result) {
+            this.isQrDialog = false
+            this.searchConditions.uuid = result
+        },
+        Error: function(message) {
+            this.$emit('Error',message)
+        }
+    },
+    watch: {
+        page : function() {
+            this.getCollections()
+        }
+    }
 }
 </script>
