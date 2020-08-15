2d05b32e1fc53e13a9fd90ecc54394e96d39823b
Sora210 <sokominto1231@gmail.com>
2020-08-16 02:22:48 +0900

    create request router

--- ManagementApiServer_ts/src/app.ts
+++ ManagementApiServer_ts/src/app.ts
@@ -3,12 +3,15 @@ import * as express from 'express';
 import { json as BodyParserJson }  from 'body-parser';
 //#################################
 //ImportRouter
+import { requestRouter } from './router/requestRouter'
 //#################################
 //MagicNumber
 const PORT:Number = 80
 //################################
 const app = express()
 app.use(BodyParserJson())
+//#################################
+app.use('/requests', requestRouter);
 //#################################
 app.listen(PORT, () => {
     console.log('Lisnig Started!!')
