9225e995caa09971b87ad956de650cdac9d57ccf
Sora210 <sokominto1231@gmail.com>
2020-08-16 02:21:23 +0900

    create Error Class

---
+++ ManagementApiServer_ts/src/error/index.ts
@@ -0,0 +1,29 @@
+class ApplicationError implements Error {
+    public name:string = 'ApplicationError';
+    constructor(public message: string) {
+        if (typeof console !== 'undefined') {
+            console.error(`ErrorName:${this.name}`);
+            console.error(`ErrorMessage:${this.message}`);
+        }
+    }
+    toString() {
+        return {
+            name: this.name,
+            message: this.message
+        };
+    };
+};
+
+class RequestError extends ApplicationError {
+    public name: string = "RequestError";
+};
+
+class NotFoundError extends ApplicationError {
+    public name: string = "NotFoundError";
+};
+
+export {
+    ApplicationError,
+    RequestError,
+    NotFoundError,
+};
