eac4f2512d73f0031b0309b1cec7d2f198b9798b
Sora210 <sokominto1231@gmail.com>
2020-08-21 02:03:20 +0900

    create docker

---
+++ ManagementApiServer_ts/Start.sh
@@ -0,0 +1,25 @@
+# /bin/sh
+
+IP="172.0.1.3"
+PORT="3306"
+WAITSET="2"
+
+
+sleep 10
+nc -vz ${IP} ${PORT} >> /dev/null
+while :
+do
+    if [ "$?" = "0" ]; then
+        echo "OK"
+        break
+    else
+        echo "NG"
+        echo "Waiting ${WAITSEC} seconds..."
+        sleep ${WAITSEC}
+        nc -vz ${IP} ${PORT}  >> /dev/null
+    fi
+done
+
+
+echo "Start Application"
+npm run start
