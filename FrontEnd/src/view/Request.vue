8c69fea65f116eb1e61c6968e69b569796ee3bc7
Sora210 <sokominto1231@gmail.com>
2020-08-22 23:58:14 +0900

    refactoring and create validation

--- FrontEnd/src/view/Request.vue
+++ FrontEnd/src/view/Request.vue
@@ -1,47 +1,45 @@
 <template>
-	<div
-		class="mt-5 mx-sm-12"
-	>
+	<v-form class="mt-5 mx-sm-12" ref="form" v-model="isValid">
 		<v-select
-			:items="items"
 			label="リクエストの種類"
+			:items="items"
+			v-model="sendData.genre"
+			:rules="[v => !!v || 'リクエストのジャンルの選択は必須です']"
 			outlined
-			append-icon="fas fa-caret-down"
-			v-model="sendData.genre">
+			append-icon="fas fa-caret-down">
 		</v-select>
 		<div v-if="sendData.genre === 0">
 			<v-text-field
-				outlined
 				label="タイトル名"
-				v-model="sendData.content">
+				v-model="sendData.content"
+				:rules="[v => !!v || 'タイトル名は必須です']"
+				outlined>
 			</v-text-field>
 		</div>
 		<div v-else-if="sendData.genre === 1">
 			<v-textarea
-				outlined 
 				label="リクエスト内容"
-				v-model="sendData.content">
+				v-model="sendData.content"
+				:rules="[v => !!v || 'リクエスト内容は必須です']"
+				outlined>
 			</v-textarea>
 		</div>
-		<div
-			class="d-flex justify-end"
-		>
+		<div class="d-flex justify-end">
 			<v-btn
+				:loading="isSending"
+				@click="sendRequest"
 				color="success"
-				class="px-6 py-2"
-				@click="SendRequest"
-				:loading="sendStatus"
-			>
+				class="px-6 py-2">
 				送信
 			</v-btn>
 		</div>
-	</div>
+	</v-form>
 </template>
 <script>
 export default {
 	data: function() {
 		return {
-			sendStatus:false,
+			isSending: false,
 			items: [
 				{
 					text: "図書リクエスト",
@@ -55,27 +53,24 @@ export default {
 			sendData: {
 				genre: "",
 				content: ""
-			}
+			},
+			isValid: true
 		};
 	},
 	methods: {
-		async SendRequest() {
-			this.sendStatus = true
-			try {
-				const apiResponse = await this.axios.post(`${this.$store.getters.apiEndpoint}/requests`,this.sendData)
-				if (apiResponse.status === 201) {
-					this.$emit('Success',"送信に成功しました")
-					this.sendStatus = false
-					this.sendData = {genre:"",content:""}
-				}
-			} catch(e) {
-				if (e.response.status === 400) {
-					this.$emit('Error',"送信に失敗しました<空白があります>")
-					this.sendStatus = false
-				} else {
-					this.sendStatus = false
-                        this.$router.push('/500')
-				}
+		async sendRequest() {
+			this.$refs.form.validate()
+			if (this.isValid) {
+				this.isSending = true
+				await this.managerApi.post('/requests', this.sendData)
+					.then(() => {
+						Object.assign(this.$data.sendData, this.$options.data().sendData)
+						this.$emit('Success',"送信に成功しました")
+					})
+					.catch(() => {
+						this.$emit('Error',"送信に失敗しました")
+					})
+				this.isSending = false
 			}
 		}
 	}
