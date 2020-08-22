84813f64f7429b38186b634843dd04c596c87307
Sora210 <sokominto1231@gmail.com>
2020-08-22 23:36:16 +0900

    API new setting

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -5,19 +5,25 @@ import router from './router/router'
 
 import vuetify from './plugins/vuetify'
 
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+import baseAxios from 'axios'
+// import VueAxios from 'vue-axios'
 import store from './store'
+
+const managerApi = baseAxios.create({
+  baseURL: process.env.VUE_APP_API_ENDPOINT
+})
 
 
 Vue.config.productionTip = false
 
-Vue.use(VueAxios, axios)
+// Vue.use(VueAxios, axios)
 
 store.commit('setFileEndpoint', process.env.VUE_APP_FILE_ENDPOINT)
-store.commit('setApiEndpoint', process.env.VUE_APP_API_ENDPOINT)
 console.log(store.getters.apiEndpoint)
 console.log(store.getters.fileEndpoint)
+
+Vue.prototype.axios = managerApi
+Vue.prototype.managerApi = managerApi
 
 new Vue({
   vuetify,
