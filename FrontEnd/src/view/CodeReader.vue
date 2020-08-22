132c75d59e89b03b66067a11cf4f3613b029c7b9
Sora210 <sokominto1231@gmail.com>
2020-08-23 00:02:17 +0900

    refactoring

--- FrontEnd/src/view/CodeReader.vue
+++ FrontEnd/src/view/CodeReader.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <QRreader :isQrDialog="isQrDialog" @dataUp="QrResult" @close="Close" @Error="Error">
+        <QRreader :isQrDialog="isQrDialog" @dataUp="catchQrResult" @close="closeQrDialog" @Error="emitError">
         </QRreader>
         <v-overlay :value="isLoading" color="white" opacity=0.7>
             <img src="../assets/book.gif">
@@ -9,45 +9,41 @@
 </template>
 <script>
 import QRreader from '../components/QRreader.vue'
-
 export default {
-    name:"CodeReader",
     components: {
         QRreader,
     },
     data: function() {
         return {
             isLoading: false,
-            historyUuid:"",
-            isQrDialog: true
+            isQrDialog: true,
+            historyUuid: '',
         }
     },
     methods: {
-        QrResult: async function(result) {
+        async catchQrResult(result) {
             this.isQrDialog = false
             this.isLoading = true
             if (this.historyUuid !== result) {
                 this.historyUuid = result
                 try {
-                    await this.axios.get(`${this.$store.getters.apiEndpoint}/collections/${result}`)
+                    await this.axios.get(`/collections/${result}`)
                     this.$router.push('/collection/' + result)
                 } catch(e) {
                     if (e.response.status === 404) {
                         this.$emit('Error',"登録されていないUUIDです")
                     } else {
-                        console.error(e)
                         this.$router.push('/500')
                     }
                     this.isQrDialog = true
                     this.isLoading = false
                 }
-                
             }
         },
-        Close: function() {
+        closeQrDialog() {
             this.$router.push('/')
         },
-        Error: function(message) {
+        emitError(message) {
             this.$emit('Error', message)
         }
     }
